28ccf704e447cf75870e3c93f77a3e21483fd424
rjindael <me@rjindael.xyz>
2026-04-13 00:06:26 -0700

    feat: wip stuffs

--- scripts/mainmenu.gd
+++ scripts/mainmenu.gd
@@ -1,5 +1,7 @@
 extends Control
 
+var verbs = ["make", "break", "shake", "rake", "take", "wake", "fake", "bake"]
+
 func _ready() -> void:
 	$Menu/SingleplayerBtn.pressed.connect(_on_singleplayer)
 	$Menu/MultiplayerBtn.pressed.connect(_on_multiplayer)
@@ -7,6 +9,9 @@ func _ready() -> void:
 	$Menu/ModsBtn.pressed.connect(_on_mods)
 	$Menu/DetailsContainer/OptionsBtn.pressed.connect(_on_options)
 	$Menu/DetailsContainer/QuitBtn.pressed.connect(_on_quit)
+	$Menu/MOTD.text = $Menu/MOTD.text % verbs.pick_random()
+
+	_play_drop_anim()
 
 func _on_singleplayer() -> void:
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
@@ -25,3 +30,7 @@ func _on_options() -> void:
 
 func _on_quit() -> void:
 	get_tree().quit()
+
+func _play_drop_anim() -> void:
+	$Menu/Logo/Block.position.y = 200
+	
